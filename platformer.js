9eac7d726bc127401ddfb7bec8743be4aa46a35c
Tara <cheeseyx@gmail.com>
2017-03-02 20:57:58 -0600

    Added circle sprite for grav objects

--- platformer.js
+++ platformer.js
@@ -41,7 +41,7 @@ function preload() {
     game.load.image('player', 'assets/player.png');
     game.load.image('exit', 'assets/exit.png');
     game.load.image('wall', 'assets/bricks_gray.png');
-    game.load.image('gravObj', 'assets/gravObj.png');
+    game.load.image('gravObj', 'assets/gravity_circle_white.png');
 
     game.load.spritesheet('shocker', 'assets/electricity_sprites.png', 30, 30, 3);
 
