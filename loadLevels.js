94cb2406c61138dc073ae217b70a3313b8fdcb5f
Amanda Nguyen <amanda@amandanguyen.com>
2017-03-10 08:14:40 -0600

    Merge branch 'master' of https://github.com/mattdavids/strong-attractor
    # Conflicts:
    #	platformer.js

--- loadLevels.js
+++ loadLevels.js
@@ -3,7 +3,7 @@ function loadLevel() {
 
     let level = levels[currentLevelNum];
     if (level == undefined) {
-        level = ['810,420','gravObj_flux,405,210, 0, 300000','wall,795,405','wall,765,405','wall,735,405','wall,735,375','wall,735,345', 'wall,765,345','wall,795,345', 'wall,795,375','exit,705,390', 'player,765,375'];
+        level = ['', '810,420','gravObj_flux,405,210, 0, 300000','wall,795,405','wall,765,405','wall,735,405','wall,735,375','wall,735,345', 'wall,765,345','wall,795,345', 'wall,795,375','exit,705,390', 'player,765,375'];
         console.log("Attempted to load undefined level");
     }
 
