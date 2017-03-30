989bc8a8cda44f5081cf3edf4917300bd91f6bd9
Amanda Nguyen <amanda@amandanguyen.com>
2017-03-29 21:53:23 -0500

    Changed menu. Will change again, but aye there's flames now.

--- gravitas/menu.js
+++ gravitas/menu.js
@@ -5,6 +5,7 @@ let Menu = function(game, startGameFromMenuCallback) {
         loadMenu: function() {
             game.load.image('background', 'assets/art/MenuBackground.png');
             game.load.image('startBtn', 'assets/art/startButton.png');
+            game.load.spritesheet('flame', 'assets/art/fire_sprites.png', 45, 60, 12);
         },
 
         createMenu: function() {
@@ -12,9 +13,23 @@ let Menu = function(game, startGameFromMenuCallback) {
             background.anchor.set(0.5, 0.5);
             background.immovable = true;
 
-            startBtn = game.add.sprite(game.width/2, game.height/2, 'startBtn');
+            startBtn = game.add.sprite(game.width/2, game.height/2 + 40, 'startBtn');
             startBtn.anchor.set(0.5, 0.5);
             startBtn.inputEnabled = true;
+            
+            
+            // THERE NEEDS TO BE A BETTER WAY TO DO THIS
+            flame_height = game.height/2 + 9;
+            
+            flame1 = game.add.sprite(97, flame_height, 'flame');
+            flame1.anchor.set(.5, .5);
+            flame1.animations.add('flicker1');
+            flame1.animations.play('flicker1', 20, true);   
+            
+            flame2 = game.add.sprite(710, flame_height, 'flame');
+            flame2.anchor.set(.5, .5);
+            flame2.animations.add('flicker2');
+            flame2.animations.play('flicker2', 20, true); 
         },
 
         onStartButtonPush: function() {
