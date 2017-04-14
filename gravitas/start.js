5ffd9c07155e1fc9ce6f65a05bc8bb4c3728e048
mattdavids <mdavids@macalester.edu>
2017-04-14 14:23:12 -0500

    Added test theme, it's alright. Needs some work

--- gravitas/start.js
+++ gravitas/start.js
@@ -4,13 +4,14 @@ let StartState = function(game) {
             // Load file lists here
             game.load.text('levelList', 'assets/levels/levelList.txt');
             
+            game.load.audio('theme', ['assets/audio/gravitasThemeTest.mp3', 'assets/audio/gravitasThemeTest.ogg']);
             //game.load.audio('sinatra', ['assets/audio/sinatra.mp3', 'assets/audio/sinatra.ogg']); // need .ogg if using Firefox
         },
         postBoot: function() {
             // Immediately run menu once boot-loading is finished
-            //let music = game.add.audio('sinatra');
-            //music.volume = 0.3;
-            //music.loop = true;
+            let music = game.add.audio('theme');
+            music.volume = 0.3;
+            music.loop = true;
             //music.play();
             
             game.state.start('menu');
