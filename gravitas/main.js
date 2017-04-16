b1427cb70cb9d7093a4ba17d1fade971db4a5157
Ilse Dippenaar <ilsedipp@gmail.com>
2017-04-16 11:37:39 -0500

    Now using buttons instead of sprites

--- gravitas/main.js
+++ gravitas/main.js
@@ -8,7 +8,7 @@ $(function() {
     let startState = new StartState(game);
 
     let menu = new Menu(game, function() {
-        game.state.start('game', startingLevelNum);
+        game.state.start('game');
     }, function() {
         game.state.start('levelselect');
     });
@@ -21,7 +21,7 @@ $(function() {
     let levelSelect = new LevelSelect(game);
     
     game.state.add('boot', {preload: startState.boot, create: startState.postBoot});
-    game.state.add('menu', {preload: menu.loadMenu, create: menu.createMenu, update: menu.onButtonPush});
+    game.state.add('menu', {preload: menu.loadMenu, create: menu.createMenu});
     game.state.add('win', {preload: win.loadWin, create: win.displayWinMessage, update: win.backToMenu});
     game.state.add('game', {preload: gameState.preload, create: gameState.create, update: gameState.update, render: gameState.render});
     game.state.add('levelselect', {preload: levelSelect.preload, create: levelSelect.create, update: levelSelect.onLevelSelected});
