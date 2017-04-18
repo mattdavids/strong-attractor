5064b82981798077e90547867ea432228ef83465
mattdavids <mdavids@macalester.edu>
2017-04-17 21:06:06 -0500

    Added a longer theme

--- gravitas/start.js
+++ gravitas/start.js
@@ -4,15 +4,15 @@ let StartState = function(game) {
             // Load file lists here
             game.load.text('levelList', 'assets/levels/levelList.txt');
             
-            game.load.audio('theme', ['assets/audio/gravitasThemeTest.mp3', 'assets/audio/gravitasThemeTest.ogg']);
-            //game.load.audio('sinatra', ['assets/audio/sinatra.mp3', 'assets/audio/sinatra.ogg']); // need .ogg if using Firefox
+            game.load.audio('theme', ['assets/audio/gravitasTheme.mp3', 'assets/audio/gravitasTheme.ogg']);
+            // need .ogg if using Firefox
         },
         postBoot: function() {
             // Immediately run menu once boot-loading is finished
             let music = game.add.audio('theme');
             music.volume = 0.3;
             music.loop = true;
-            //music.play();
+            music.play();
             
             game.state.start('menu');
         }
