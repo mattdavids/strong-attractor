cf03e858ee41f75b6210d80930d999af64e239ad
mattdavids <mdavids@macalester.edu>
2017-04-28 22:19:18 -0500

    Added shadow state that sets up and updates shadows

--- gravitas/game.js
+++ gravitas/game.js
@@ -12,12 +12,6 @@ let Game = function (game) {
         movers,
         tutorialSigns;
 
-    // Player collision shadows
-    let playerShadowLeft,
-        playerShadowRight,
-        playerShadowBottom,
-        playerShadowTop;
-
     let clickedObj;
 
     // Dynamic displayables
@@ -43,6 +37,7 @@ let Game = function (game) {
     let deathState;
     let exitState;
     let freezeState;
+    let shadowState;
     
     let playerDataList = [];
 
@@ -245,6 +240,7 @@ let Game = function (game) {
         deathState = new DeathState();
         exitState = new ExitState();
         freezeState = new FreezeState();
+        shadowState = new ShadowState();
 
         loadLevel();
 
@@ -259,30 +255,7 @@ let Game = function (game) {
             }
         };
 
-        //makeLevelSelector();
-
-        playerShadowLeft = game.add.sprite(player.body.position.x, player.body.position.y, 'shadow');
-        playerShadowLeft.anchor.set(.5, .5);
-        playerShadowLeft.body.setSize(5, 1, 0, 8);
-
-        playerShadowRight = game.add.sprite(player.body.position.x, player.body.position.y, 'shadow');
-        playerShadowRight.anchor.set(.5, .5);
-        playerShadowRight.body.setSize(15, 1, 0, 8);
-
-        playerShadowBottom = game.add.sprite(player.body.position.x, player.body.position.y, 'shadow');
-        playerShadowBottom.anchor.set(.5, .5);
-        playerShadowBottom.body.setSize(15, 1, 0, 14);
-
-        playerShadowTop = game.add.sprite(player.body.position.x, player.body.position.y, 'shadow');
-        playerShadowTop.anchor.set(.5, .5);
-        playerShadowTop.body.setSize(13, 1, 0, 0);
-
-        notCurrentlyExiting = true;
-        exitTick = 0;
-        
-        notCurrentlyDying = true;
-        deathFall = false;
-        diedRecently = false;
+        shadowState.setUp(game, player);
 
         rightKeyWasPressed = false;
         leftKeyWasPressed = false;
@@ -454,29 +427,8 @@ let Game = function (game) {
                     p.life = 0;
             }, null, null);
         }, null);
-
-        player.isTouchingRight = false;
-        player.isTouchingLeft = false;
-        player.isTouchingBottom = false;
-        player.isTouchingTop = false;
         
-        playerShadowLeft.body.position.set(player.body.position.x - 2, player.body.position.y);
-        playerShadowRight.body.position.set(player.body.position.x + .5, player.body.position.y);
-        playerShadowBottom.body.position.set(player.body.position.x - 1, player.body.position.y + 15);
-        playerShadowTop.body.position.set(player.body.position.x + 1, player.body.position.y - 17);
-
-        game.physics.arcade.overlap(playerShadowRight, walls, function() {
-            player.isTouchingRight = true;
-        }, null, null);
-        game.physics.arcade.overlap(playerShadowLeft, walls, function() {
-            player.isTouchingLeft = true;
-        }, null, null);
-        game.physics.arcade.overlap(playerShadowBottom, walls, function() {
-            player.isTouchingBottom = true;
-        }, null, null);
-        game.physics.arcade.overlap(playerShadowTop, walls, function() {
-            player.isTouchingTop = true;
-        }, null, null);
+        shadowState.update(game, player, walls);
         
         // If the player is not dead, play the death animation on contact with shockers or the exit animation on contact with an exit
         if (deathState.notCurrentlyDying && !deathState.diedRecently && exitState.notCurrentlyExiting) {
