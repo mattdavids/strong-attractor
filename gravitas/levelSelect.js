73fc4b5a94ecc372ead2e5985018e3b868d0f277
Amanda Nguyen <amanda@amandanguyen.com>
2017-05-02 00:53:13 -0500

    Added more functions. WIP.

--- gravitas/levelSelect.js
+++ gravitas/levelSelect.js
@@ -1,18 +1,48 @@
 let LevelSelect = function (game, gameState) {
     
-    let map,
+    let levelSelectBackground,
         startX, // used to save start touch position
         startY, // used to save start touch position
-        mapSpeed = 1,
-        mapGroup,
+        scrollSpeed = 1,
+        levelGroup,
         selectedLevel;
 
     function loadLevelSelect() {
+        game.load.image('levelSelectBackground', 'assets/art/levelSelectImages/LevelSelectBackground.png');
+        game.load.image('level', 'assets/art/levelSelectImages/levelThumbnail');
+    }
+
+    function createLevelSelect() {
+        updateFromLocalStorage();
+        renderLevelSelect();
+    }
+
+    function updateFromLocalStorage() {
+        playerDataList = localStorage.getItem('user_progress');
+        if (playerDataList == null) {
+            playerDataList = [0];
+            for (let i = 1; i < levelCount; i++) {
+                playerDataList[i] = 1;
+            }
+            localStorage.setItem('user_progress', playerDataList);
+        } else {
+            playerDataList = playerDataList.split(',');
+        }
+    }
+
+    function renderLevelSelect() {
+        background = game.add.image(game.width/2, game.height/2, 'levelSelectBackground');
+        background.anchor.set(0.5, 0.5);
+        background.immovable = true;
+
+        levelGroup = game.add.group();
+
 
     }
 
-    function createLevel() {
-        
+    function clearLevel() {
+        background.kill();
+        levelGroup.destroy();
     }
     
     return {
