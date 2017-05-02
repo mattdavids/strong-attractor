82d020c8edff6a325f369193836c1b415239448b
Amanda Nguyen <amanda@amandanguyen.com>
2017-05-01 22:45:15 -0500

    Changing level select format to include scroll.

--- gravitas/levelSelect.js
+++ gravitas/levelSelect.js
@@ -1,90 +1,18 @@
 let LevelSelect = function (game, gameState) {
     
-    let thumbCols = 2,
-        thumbRows = 5,
-        thumbWidth = 96,
-        thumbHeight = 96,
-        thumbSpacing = 20,
-        levelWidth = thumbWidth * thumbRows + thumbSpacing * (thumbRows - 1),
-        levelHeight = thumbHeight * thumbCols + thumbSpacing * (thumbCols - 1),
-        playerDataList = [],
-        buttons,
-        levelCount;
-    
- 
+    let map,
+        startX, // used to save start touch position
+        startY, // used to save start touch position
+        mapSpeed = 1,
+        mapGroup,
+        selectedLevel;
+
     function loadLevelSelect() {
-        let levelList = game.cache.getText('levelList').split('\n');
-        levelCount = levelList.length;
-        
-        game.load.image('lockedThumbnail', 'assets/art/levelSelectImages/locked.png', thumbHeight, thumbWidth);
-        game.load.image('levelSelectBackground', 'assets/art/LevelSelectBackground.png');
-        
-        // level thumbnails - would rather use spritesheet
-        for (let i = 1; i <= levelCount; i++) { 
-            game.load.image('thumbnail' + i, 'assets/art/levelSelectImages/thumbnail' + i + '.png', thumbHeight, thumbWidth);
-        }
-    } 
-    
-    function updateFromLocalStorage() {
-        playerDataList = localStorage.getItem('user_progress');
-        if (playerDataList == null) {
-            playerDataList = [0];
-            for (let i = 1; i < levelCount; i++) {
-                playerDataList[i] = 1;
-            }
-            localStorage.setItem('user_progress', playerDataList);
-        } else {
-            playerDataList = playerDataList.split(',');
-        }
+
     }
-    
-    function createLevelSelect() {
-        updateFromLocalStorage();
-        renderLevelSelect();
-    }
-    
-    function clearLevel() {
-        background.kill();
-        buttons.destroy();
-    }
-    
-    function renderLevelSelect() {
-        background = game.add.sprite(game.width/2, game.height/2, 'levelSelectBackground');
-        background.anchor.set(0.5, 0.5);
-        background.immovable = true;
+
+    function createLevel() {
         
-        buttons = game.add.group();
-        
-        // horizontal offset to have lock thumbnails horizontally centered in the page
-        let offsetX = (game.width - levelWidth)/2;
-        let offsetY = (game.height - levelHeight)/2 + 60;
-        
-        let associatedLevel = 0;
-        for (let i = 0; i < thumbCols; i++) {
-            for (let j = 0; j < thumbRows; j++) {
-                if (associatedLevel != levelCount) {
-                    
-                    let button;
-                    
-                    if (playerDataList[associatedLevel] == 0) {
-                        // level is unlocked
-                        button = game.add.button(offsetX + j * (thumbWidth + thumbSpacing), offsetY + i * (thumbHeight + thumbSpacing), 'thumbnail' + (associatedLevel + 1), function(){
-                            clearLevel();
-                            gameState.setLevel(button.associatedLevel);
-                            game.state.start('game');
-                        });
-                    } else {
-                        // level is locked
-                        button = game.add.button(offsetX + j * (thumbWidth + thumbSpacing), offsetY + i * (thumbHeight + thumbSpacing), 'lockedThumbnail', function() {
-                            alert("This level is locked until completed.");
-                        });
-                    }
-                    button.associatedLevel = associatedLevel;
-                    buttons.add(button);
-                    associatedLevel++;
-                }
-            } 
-        }
     }
     
     return {
