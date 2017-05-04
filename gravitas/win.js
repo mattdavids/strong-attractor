cc7aad13d30632954a543d285cad2805310107ba
Amanda Nguyen <amanda@amandanguyen.com>
2017-05-04 01:23:04 -0500

    Made flag in win screen bigger.

--- gravitas/win.js
+++ gravitas/win.js
@@ -6,6 +6,7 @@ let Win = function(game, returnFromWinCallback) {
 
     function backToMenu() {
         winScreen.kill();
+        flag.kill();
         menuButton.kill();
         returnFromWinCallback()
     }
@@ -14,7 +15,7 @@ let Win = function(game, returnFromWinCallback) {
         loadWin: function() {
             game.load.image('winScreen', 'assets/art/winScreen.png');
             game.load.image('menuButton', 'assets/art/menuButton.png');
-            game.load.spritesheet('flag', 'assets/art/flagSpritesheet.png', 80, 135, 10);
+            game.load.spritesheet('flag', 'assets/art/flagSpritesheet.png', 85, 152, 9);
         },
 
         displayWinMessage: function() {
@@ -23,10 +24,10 @@ let Win = function(game, returnFromWinCallback) {
             winScreen.anchor.set(0.5, 0.5);
             winScreen.immovable = true;
 
-            menuButton = game.add.button(game.width/2, 1.5*game.height/2, 'menuButton', backToMenu);
+            menuButton = game.add.button(game.width/2, 1.4*game.height/2, 'menuButton', backToMenu);
             menuButton.anchor.set(0.5, 0.5);
 
-            flag = game.add.sprite(360, 46, 'flag');
+            flag = game.add.sprite(360, 30, 'flag');
             displayFlag = flag.animations.add('displayFlag');
             displayFlag.enableUpdate = true;
             flag.animations.play('displayFlag', 10, false);
