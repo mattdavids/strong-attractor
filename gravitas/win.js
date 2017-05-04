7e0f6f9e7dd007f31ceea219391c8d4a78478c05
mattdavids <mdavids@macalester.edu>
2017-05-04 15:13:09 -0500

    Added fireworks to demo

--- gravitas/win.js
+++ gravitas/win.js
@@ -1,20 +1,49 @@
-let Win = function(game, returnFromWinCallback) {
+let Win = function(game) {
     let winScreen,
         menuButton,
         flag,
-        displayFlag;
+        displayFlag,
+        emitters;
 
     function backToMenu() {
         winScreen.kill();
         flag.kill();
         menuButton.kill();
-        returnFromWinCallback()
+        game.state.start('menu');
+    }
+    
+    function quadraticEase(t, tmax) {
+        return 1 - Math.pow(tmax - t, 2)/Math.pow(tmax, 2);
+    }
+    
+    function update() {
+        if(Math.random() > .95) {
+            let emitter = game.add.emitter(Math.random() * 600 + 100, Math.random() * 100 + 45);
+            let numParticles = Math.random() * 30 + 20;
+
+            emitter.makeParticles('gravParticle', 0, numParticles, true);
+            emitter.minParticleScale = .3;
+            emitter.maxParticleScale = .5;
+            emitter.width = 20;
+            emitter.start(true, 2000, null, numParticles);
+            game.time.events.add(2500, function() {
+                emitter.destroy(true);
+            }, null);
+            emitters.add(emitter);
+        }
+        
+        emitters.forEach(function(emitter) {
+            emitter.forEachAlive(function(p) {
+                p.alpha = quadraticEase(p.lifespan, emitter.lifespan);
+            }, null);
+        }, null);
     }
 
     return {
         loadWin: function() {
             game.load.image('winScreen', 'assets/art/winScreen.png');
             game.load.image('menuButton', 'assets/art/menuButton.png');
+            game.load.image('gravParticle', 'assets/art/gravParticle.png');
             game.load.spritesheet('flag', 'assets/art/flagSpritesheet.png', 85, 152, 9);
         },
 
@@ -31,7 +60,11 @@ let Win = function(game, returnFromWinCallback) {
             displayFlag = flag.animations.add('displayFlag');
             displayFlag.enableUpdate = true;
             flag.animations.play('displayFlag', 10, false);
-        }
+            
+            emitters = game.add.group();
+            
+        },        
+        update: update,
 
     }
 };
