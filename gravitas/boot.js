821e732e66dc5ee42a9c6b093310647bc2a59467
mattdavids <mdavids@macalester.edu>
2017-05-08 13:30:11 -0500

    Game music only fades in the first time

--- gravitas/boot.js
+++ gravitas/boot.js
@@ -12,33 +12,34 @@ let BootState = function(game) {
             
             game.load.audio('theme', ['assets/audio/gravitasTheme.mp3', 'assets/audio/gravitasTheme.ogg']);
             // need .ogg if using Firefox
-            
+
             game.stage.smoothed = false;
             resizeGame();
             game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
             
         },
         postBoot: function() {
-            // Immediately run menu once boot-loading is finished
-            //music = game.add.audio('theme');
-            //music.volume = 0.3;
-            //music.loop = true;
-            //music.onDecoded.add(function() {
-            //    music.fadeIn(1000);
-            //});
             
-            // Pause music when the window loses focus
-            $(window).focus(function() {
-                $('#mainTheme')[0].play();
-                $('#slowTheme')[0].play();
+            $(document).ready(function() {
+                
+                // Pause music when the window loses focus
+                let mainTheme = $('#mainTheme');
+                $(window).focus(function() {
+                    mainTheme[0].play();
+                });
+                $(window).blur(function() {
+                    mainTheme[0].pause();
+                });
+                
+                //On window resize, resize the game
+                $(window).resize(resizeGame);
+                
+                mainTheme.prop("volume", 0.1);
+                mainTheme.trigger("play");
+                mainTheme.animate({volume: 1}, 1500);
+                
+                game.state.start('menu');
             });
-            $(window).blur(function() {
-                $('#mainTheme')[0].pause();
-                $('#slowTheme')[0].pause();
-            });
-            $(window).resize(resizeGame);
-            
-            game.state.start('menu');
         }
     };
 };
